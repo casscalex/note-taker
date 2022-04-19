83a3065961ebe53d30586a5d2cb1048da15e55e0
casscalex <casscalex@outlook.com>
2022-04-18 23:22:55 -0500

    keep getting an error can't find routes

--- Main/routes/apiRoutes.js
+++ Main/routes/apiRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
+let db = require('../db/db.json');
+const fs = require('fs');
 
 router.get('/notes', (req, res) => {
-        return res.json(notes);
-    })
+ let results = db;
+ res.json(results);
+});
+
+router.post('/notes', (req,res) =>  {
+    res.json(db)
 })
+
+router.delete('/notes', (req, res) => {
+
+})
+
+fs.writeFile('./db/db.json', JSON.stringify(db), (err) =>
+err ? console.error(err) : console.log('notes has been written'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
+const path = require('path');
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
-// Initialize the app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Server on PORT: ${PORT}!`));
